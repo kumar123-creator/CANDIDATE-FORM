13c6fc6fdbe467004e51aea1c0d8a72d8b11fb15
kumar123-creator <132423913+kumar123-creator@users.noreply.github.com>
2023-05-30 10:17:46 +0530

    code modified

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,15 @@
 <script>
+  import { onMount } from "svelte";
+  import { createEventDispatcher } from "svelte";
+  import 'bootstrap/dist/css/bootstrap.min.css';
+
   let jsonData = [];
   let tableVisible = false;
   let selectedCandidate = null;
+  let editedCandidate = null;
+  let showSuccessMessage = false;
+
+  const dispatch = createEventDispatcher();
 
   onMount(async () => {
     await fetchData();
@@ -9,27 +17,35 @@
   });
 
   async function fetchData() {
-    const response = await fetch("https://api.recruitly.io/api/candidate?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E");
+    const response = await fetch("https://api.recruitly.io/api/candidate/?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E");
     const responseData = await response.json();
     jsonData = responseData.data;
   }
 
   function handleTitleClick(candidate) {
     selectedCandidate = candidate;
+    editedCandidate = { ...selectedCandidate };
   }
 
-  function handleFieldChange(event, field) {
-    selectedCandidate[field] = event.target.value;
-  }
+  async function saveCandidate() {
+    Object.assign(selectedCandidate, editedCandidate);
 
-  function handleSaveClick() {
+    // Find the index of the selectedCandidate in the jsonData array
     const index = jsonData.findIndex(candidate => candidate.id === selectedCandidate.id);
 
-    if (index !== -1) {
-      jsonData[index] = selectedCandidate;
-    }
+    // Update the corresponding item in the jsonData array
+    jsonData[index] = { ...selectedCandidate };
 
+    // Send the updated candidate data to the API
+
+    showSuccessMessage = true;
+
+    closePopup();
+  }
+
+  function closePopup() {
     selectedCandidate = null;
+    editedCandidate = null;
   }
 </script>
 
@@ -47,73 +63,123 @@
   }
 
   .popup-content {
-    background-color: #fff;
-    padding: 20px;
-    border-radius: 5px;
+    background-color: #ffffff;
+    padding: 40px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
     max-width: 400px;
+    width: 90%;
     text-align: center;
   }
 
-  .popup-content h2 {
-    margin-bottom: 10px;
+  .popup-content h1 {
+    color: blue;
+    font-size: 24px;
+    margin-bottom: 25px;
   }
 
   .popup-content p {
-    margin-bottom: 5px;
+    margin-bottom: 10px;
   }
 
   .popup-content input {
     width: 100%;
-    padding: 5px;
-    margin-bottom: 10px;
+    padding: 7px;
+    margin-bottom: 15px;
   }
 
   .popup-content button {
     margin-top: 20px;
   }
+
+  .success-popup {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .success-popup-content {
+    background-color: #ffffff;
+    padding: 40px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    max-width: 400px;
+    width: 90%;
+    text-align: center;
+  }
+
+  .success-popup-content p {
+    margin-bottom: 10px;
+  }
+
+  .success-popup-content button {
+    margin-top: 20px;
+  }
+
+  .success-popup-content h1 {
+    color: green;
+    font-size: 24px;
+    margin-bottom: 25px;
+  }
 </style>
 
-<main>
+<main class="container mt-4">
   {#if tableVisible}
-    <table class="table">
-      <thead class="thead-light">
-        <tr>
-          <th>First Name</th>
-          <th>Surname</th>
-          <th>Email</th>
-          <th>Mobile</th>
-        </tr>
-      </thead>
-      <tbody>
-        {#each jsonData as candidate}
+    <div class="table-container">
+      <table class="table">
+        <thead class="thead-light">
           <tr>
-            <td>
-              <a href="#" on:click|preventDefault={() => handleTitleClick(candidate)}>{candidate.firstName}</a>
-            </td>
-            <td>{candidate.surname}</td>
-            <td>{candidate.email}</td>
-            <td>{candidate.mobile}</td>
+            <th>First Name</th>
+            <th>Surname</th>
+            <th>Email</th>
+            <th>Mobile</th>
           </tr>
-        {/each}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {#each jsonData as candidate}
+            <tr>
+              <td>
+                <a href="#" on:click|preventDefault={() => handleTitleClick(candidate)}>{candidate.firstName}</a>
+              </td>
+              <td>{candidate.surname}</td>
+              <td>{candidate.email}</td>
+              <td>{candidate.mobile}</td>
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+    </div>
   {/if}
 </main>
 
 {#if selectedCandidate}
-  <div class="popup">
-    <div class="popup-content">
-      <h2>Edit Candidate</h2>
-      <label for="firstName">First Name:</label>
-      <input type="text" id="firstName" bind:value={selectedCandidate.firstName} on:input={(e) => handleFieldChange(e, 'firstName')} />
-      <label for="surname">Surname:</label>
-      <input type="text" id="surname" bind:value={selectedCandidate.surname} on:input={(e) => handleFieldChange(e, 'surname')} />
-      <label for="email">Email:</label>
-      <input type="text" id="email" bind:value={selectedCandidate.email} on:input={(e) => handleFieldChange(e, 'email')} />
-      <label for="mobile">Mobile:</label>
-      <input type="text" id="mobile" bind:value={selectedCandidate.mobile} on:input={(e) => handleFieldChange(e, 'mobile')} />
-      <button class="btn btn-primary" on:click={handleSaveClick}>Save</button>
-      <button class="btn btn-secondary" on:click={() => (selectedCandidate = null)}>Close</button>
+<div class="popup">
+  <div class="popup-content">
+    <h1 style="color:blue;">Candidate Details</h1>
+    <p>First Name: <input type="text" bind:value={editedCandidate.firstName} placeholder="Enter first name" /></p>
+    <p>Surname: <input type="text" bind:value={editedCandidate.surname} placeholder="Enter surname" /></p>
+    <p>Email: <input type="text" bind:value={editedCandidate.email} placeholder="Enter email" /></p>
+    <p>Mobile: <input type="text" bind:value={editedCandidate.mobile} placeholder="Enter mobile" /></p>
+    <div>
+      <button class="btn btn-primary" on:click={saveCandidate}>Save</button>
+      <button class="btn btn-secondary" on:click={closePopup}>Close</button>
     </div>
   </div>
+</div>
+{/if}
+
+{#if showSuccessMessage}
+<div class="success-popup">
+  <div class="success-popup-content">
+    <h1>Success!</h1>
+    <p>Your details have been successfully updated.</p>
+    <button class="btn btn-primary" on:click={() => showSuccessMessage = false}>Close</button>
+  </div>
+</div>
 {/if}
